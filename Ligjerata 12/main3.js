cddf9be420b9a7420ecf1a04a76524c640343f50
EltonPajaziti <pajazitielton002@gmail.com>
2024-11-04 12:11:21 +0100

    Module 26 intro
    intro

---
+++ Ligjerata 12/main3.js
@@ -0,0 +1,14 @@
+var x=10;
+var y=5;
+
+if(x>y){
+    console.log("This is true");
+}else{
+    console.log("This is wrong");
+}
+
+if(x==7){
+    console.log("10 equals 7");
+}else{
+    console.log("10 doesnt equal 7");
+}
