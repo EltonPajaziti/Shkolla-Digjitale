a21688bc1c2e5ccfad8a8b738ba720d9a5bf9c8f
EltonPajaziti <pajazitielton002@gmail.com>
2024-11-25 11:40:59 +0100

    Update index.php

---
+++ PHP/module 40/index.php
@@ -0,0 +1,30 @@
+<?php
+$pershendetje="HELLO!";
+echo $pershendetje;
+echo "<br>";
+$name="Elton";
+echo"Hello I am $name  <br>";
+echo "Hello I am ". $name."<br>";//concatonation bohet me ane te pikes jo me +
+$x=50;
+$y=30;
+echo $x + $y ;
+echo "<br>";
+echo $x - $y;
+echo "<br>";
+echo $x * $y;
+echo "<br>";
+echo $x / $y;
+echo "<br>";
+
+$the_string= "Elton Pajaziti";
+echo strlen($the_string)."<br>";
+echo str_word_count($the_string)."<br>";
+echo str_replace("Pajaziti","Bossi",$the_string);
+echo "<br>";
+
+
+
+
+
+
+?>
