aaf7d9548ef64bbe3299fbcf5e34291cb5dd1301
EltonPajaziti <pajazitielton002@gmail.com>
2024-12-02 10:51:38 +0100

    Update index.php

--- PHP/module 45/index.php
+++ PHP/module 45/index.php
@@ -4,7 +4,7 @@ $user='root';
 $pass='';
 
 try{
-    $conn= new PDO("mysql:host= $host;", $user, $pass);
+    $conn= new PDO("mysql:host=$host;", $user, $pass);
     echo "Connected!";
 } catch( Exception $e){
     echo"Not Connected";
