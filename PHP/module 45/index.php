377d747f7318382cfa12d41423becd633f5fbcd7
EltonPajaziti <pajazitielton002@gmail.com>
2024-12-02 12:19:49 +0100

    Update index.php

--- PHP/module 45/index.php
+++ PHP/module 45/index.php
@@ -1,13 +1,44 @@
 <?php
-$host='localhost';
+$host='localhost:4307';
 $user='root';
 $pass='';
+$db='shkolladigjitale';
 
 try{
-    $conn= new PDO("mysql:host=$host;", $user, $pass);
-    echo "Connected!";
+    $conn= new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+
+    // $sql= "CREATE TABLE users(id INT(6) NOT NULL AUTO_INCREMENT  PRIMARY KEY,
+    // username VARCHAR(255) NOT NULL,
+    // password VARCHAR(255) NOT NULL) ";
+
+
+    
+
+    // // $username= "Elton";
+    // $password= password_hash("12345678",PASSWORD_DEFAULT);
+    // $sql = "INSERT INTO users (username, password) VALUES ('Djellza', '$password')";
+
+    // $sql="INSERT INTO users(username, password) VALUES ('$username','$password')";
+
+    // $sql="ALTER TABLE users ADD email varchar(255)";
+
+    //deleting a column
+    // $sql="ALTER TABLE users DROP column email";
+
+
+
+
+
+    
+    $conn->exec($sql);
+    $last_id = $conn->lastInsertId();
+ 
+    // echo "User added successfully! User ID: " . $last_id;
+    echo "COLUMN is deleted";
+    echo "<br>";
+ 
 } catch( Exception $e){
-    echo"Not Connected";
+    echo"Something went wrong!";
 }
 
 
